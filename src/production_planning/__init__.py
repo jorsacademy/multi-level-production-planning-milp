e197c85d070119bcf2d025724bb27bdf0335da52
Jors Academy <abalci16@posta.pau.edu.tr>
2026-09-13 13:16:02 +0300

    Export capacity calendar adapter

--- src/production_planning/__init__.py
+++ src/production_planning/__init__.py
@@ -2,6 +2,7 @@ from .optimizer import ProductionPlanner
 from .bom import explode_bom, validate_bom_acyclic
 from .atp import available_to_promise
 from .pegging import peg_orders
+from .calendar import capacity_calendar_to_periods
 
 __all__ = [
     "ProductionPlanner",
@@ -9,4 +10,5 @@ __all__ = [
     "validate_bom_acyclic",
     "available_to_promise",
     "peg_orders",
+    "capacity_calendar_to_periods",
 ]
